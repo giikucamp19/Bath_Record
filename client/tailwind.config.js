e99314854bb07eb10c2f1b9a8d73a9cb484ff5af
杉浦 <130259690+X22054@users.noreply.github.com>
2024-12-17 23:18:11 +0900

    カレンダー機能を追加しました。

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/components/Home.jsx", // Tailwindが適用されるファイルを指定
+    "./src/components/Notice.jsx"
+  ],
   theme: {
     extend: {},
   },
